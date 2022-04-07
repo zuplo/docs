32651cc0813151875b5964eab44b4805398548b8
Nate Totten <nate@zuplo.com>
2022-04-07 16:46:13 -0400

    removed embed

--- embed/components/articles/icons.tsx
+++
@@ -1,5 +0,0 @@
-import { LightningBoltIcon } from "@heroicons/react/outline";
-
-export const RouteTesterIcon = () => (
-  <LightningBoltIcon className="inline-block h-6 w-6" />
-);
--- embed/components/ribbon.tsx
+++
@@ -1,6 +0,0 @@
-import ribbonStyles from "./ribbon.module.css";
-export const Ribbon = () => (
-  <div className={ribbonStyles["ribbon"]}>
-    <span>Coming Soon</span>
-  </div>
-);
