80dff4003679a898796810422383cd713f36867f
Nate Totten <nate@zuplo.com>
2022-04-07 16:46:13 -0400

    removed embed

--- embed/lib/context.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ArticleScope } from "./interfaces";
-
-export const MdxScopeContext = React.createContext<ArticleScope>({
-  embed: false,
-});
--- embed/scripts/local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ln -s $PWD/../docs/media $PWD/public/media
-
-npm run reference:ci
-
-echo "Local setup complete"
