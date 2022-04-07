07a9776935c3f0c4ae9c104e22da2a3f20abf903
Nate Totten <nate@zuplo.com>
2022-04-07 18:05:09 -0400

    added embeds

---
+++ quickstarts/index.tsx
@@ -0,0 +1,54 @@
+import * as fs from "fs";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import EmbedPage from "./page";
+import * as path from "path";
+import { JSDOM } from "jsdom";
+
+const buildDir = path.resolve(__dirname, "../build");
+const outputDir = path.resolve(__dirname, "dist");
+if (fs.existsSync(outputDir)) {
+  fs.rmSync(outputDir, { recursive: true, force: true });
+}
+fs.mkdirSync(outputDir);
+
+const qsPath = path.resolve(buildDir, "docs/quickstarts");
+const qsDirs = fs.readdirSync(qsPath);
+qsDirs.forEach((dir) => {
+  const htmlPath = path.resolve(qsPath, dir, "index.html");
+  if (fs.existsSync(htmlPath)) {
+    const html = fs.readFileSync(htmlPath, "utf-8");
+    const dom = new JSDOM(html);
+    const el = dom.window.document.querySelector("div.markdown");
+    if (!el) {
+      throw new Error("Cannot find markdown div");
+    }
+    const qsOutputPath = path.resolve(outputDir, dir);
+    fs.mkdirSync(qsOutputPath);
+    render(el.innerHTML, path.join(qsOutputPath, "index.html"));
+  }
+});
+
+// render();
+
+function render(content: string, outputPath: string) {
+  let html = ReactDOMServer.renderToStaticMarkup(<Page content={content} />);
+  let htmlWDoc = "<!DOCTYPE html>" + html;
+  fs.writeFileSync(outputPath, htmlWDoc);
+  console.log(`Wrote ${outputPath}`);
+}
+
+function Page({ content }: { content: string }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Zuplo Quickstarts</title>
+        <link rel="stylesheet" href="../index.css" />
+      </head>
+      <body>
+        <EmbedPage content={content} />
+      </body>
+    </html>
+  );
+}
