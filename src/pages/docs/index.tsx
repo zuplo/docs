56172ec11726618f58ae08b17860c8c1713a22a4
Nate Totten <nate@zuplo.com>
2022-04-09 11:50:53 -0400

    home page changes

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -5,20 +5,37 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import * as d from "@docusaurus/utils";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className={styles.heroIntro}>
+          Check out our <Link to="/docs/quickstarts">quick starts</Link>, browse{" "}
+          <Link to="/docs/examples">examples</Link>, or learn{" "}
+          <Link to="/docs/overview">how Zuplo can help</Link> you share your
+          API. Connect with the team on{" "}
+          <Link to="https://discord.gg/W36Ddfcd" target="_blank">
+            Discord
+          </Link>
+          .
+        </p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/overview"
+            className="button button--primary button--lg"
+            to="/docs/quickstarts"
           >
             Get Started &nbsp;&nbsp;→
+          </Link>
+          <Link
+            className="button button--secondary button--lg margin-left--md"
+            to="/docs/examples"
+          >
+            Examples
           </Link>
         </div>
       </div>
