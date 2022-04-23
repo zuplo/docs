2252725983b9e962ce16321605189534d3c0cb07
Nate Totten <nate@zuplo.com>
2022-04-23 17:35:20 -0400

    quickstart work

--- docs/quickstarts/setup-your-api-proxy.md
+++ docs/quickstarts/setup-your-api-proxy.md
@@ -2,3 +2,10 @@
 sidebar_position: 1
 title: Setup Your API Proxy
 ---
+
+import QuickstartPicker from "@site/src/components/QuickstartPicker";
+
+Zuplo isn't your average gateway. It's a **programmable gateway** that can be
+used to protect and share your own API _and_ is the easiest way to configure API Key Management and API Docs. Choose your getting started guide:
+
+<QuickstartPicker slug="quickstarts/setup-your-api-proxy" />
--- embed/index.tsx
+++ embed/index.tsx
@@ -3,7 +3,6 @@ import { JSDOM } from "jsdom";
 import * as path from "path";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-import QuickstartPicker from "./quickstart-picker";
 
 async function run() {
   const embedPath = path.resolve(__dirname, "../build/embed/");
@@ -29,19 +28,24 @@ async function run() {
         const article = document.querySelector("article div.markdown");
         const css = document.querySelector('link[rel="stylesheet"]');
 
-        const title = article.querySelector("h1").textContent;
-        article.querySelector("header").remove();
-
         const links = article.getElementsByTagName("a");
+
         for (let i = 0; i < links.length; i++) {
-          links[i].setAttribute("target", "_blank");
+          const href = links[i]
+            .getAttribute("href")
+            .replace("/docs/quickstarts/", "/embed/quickstarts/");
+
+          if (href.startsWith("/embed/quickstarts/")) {
+            links[i].setAttribute("href", href);
+          } else {
+            links[i].setAttribute("target", "_blank");
+          }
         }
 
         const outputDir = path.join(embedQuickstartPath, dir);
         await fs.mkdir(outputDir, { recursive: true });
         const outputPath = path.join(outputDir, "index.html");
         await render(outputPath, {
-          title,
           content: article.innerHTML,
           slug: `/quickstart/${dir}`,
           defaultCssUrl: css.getAttribute("href"),
@@ -53,7 +57,6 @@ async function run() {
 }
 
 interface PageProps {
-  title: string;
   content: string;
   slug: string;
   defaultCssUrl: string;
@@ -67,12 +70,12 @@ async function render(outputPath: string, props: PageProps) {
   console.log(`Wrote ${outputPath}`);
 }
 
-function Page({ title, content, slug, defaultCssUrl, embedCssUrl }: PageProps) {
+function Page({ content, slug, defaultCssUrl, embedCssUrl }: PageProps) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Zuplo Quickstarts | {title}</title>
+        <title>Zuplo Quickstarts</title>
         <link rel="stylesheet" href={defaultCssUrl} />
         <link rel="stylesheet" href={embedCssUrl} />
         <link rel="canonical" href={`https://zuplo.com/docs${slug}`} />
@@ -80,19 +83,10 @@ function Page({ title, content, slug, defaultCssUrl, embedCssUrl }: PageProps) {
       </head>
       <body>
         <div className="container">
-          <article>
-            <h1>{title}</h1>
-            <p>
-              Zuplo isn't your average gateway. It's a{" "}
-              <strong>programmable gateway</strong> that is the easiest way to
-              share and secure your APIs. Choose your getting started guide:
-            </p>
-            <QuickstartPicker slug={slug} />
-            <div
-              className="theme-doc-markdown markdown"
-              dangerouslySetInnerHTML={{ __html: content }}
-            ></div>
-          </article>
+          <article
+            className="theme-doc-markdown markdown"
+            dangerouslySetInnerHTML={{ __html: content }}
+          ></article>
         </div>
       </body>
     </html>
