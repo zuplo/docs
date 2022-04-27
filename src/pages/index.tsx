f4a1bb74364e782800bb8f2357ad5ddfc3a2cdf9
Nate Totten <nate@zuplo.com>
2022-04-27 08:55:36 -0400

    redirect home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
+import { Redirect } from "@docusaurus/router";
 import React from "react";
 
 // This page never gets hit in production,
 // this is just a helper for local dev
 export default function Home() {
-  return (
-    <div>
-      <a href="/docs">Debug page only. Go to Docs</a>
-    </div>
-  );
+  return <Redirect to="/docs" />;
 }
