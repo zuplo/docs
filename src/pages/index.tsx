a096f0a779f8ce48b7bcf6c8c1f096b69cae284c
Nate Totten <nate@zuplo.com>
2022-04-27 08:53:58 -0400

    updated sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import BrowserOnly from "@docusaurus/BrowserOnly";
 import React from "react";
 
 // This page never gets hit in production,
 // this is just a helper for local dev
 export default function Home() {
-  <BrowserOnly>
-    {() => {
-      window.location.href = "/docs";
-      return <></>;
-    }}
-  </BrowserOnly>;
-  return <div></div>;
+  return (
+    <div>
+      <a href="/docs">Debug page only. Go to Docs</a>
+    </div>
+  );
 }
