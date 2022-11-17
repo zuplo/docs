25c2e21455c5c92fe1760a78b1b1550dbbc81efe
Nathan Totten <nathan@ntotten.com>
2022-11-17 16:48:33 -0500

    Removed search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,29 +76,6 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       image: "https://cdn.zuplo.com/static/social/img-twitter-header.jpg",
-      algolia: {
-        // The application ID provided by Algolia
-        appId: "E7EUGPZYED",
-
-        // Public API key: it is safe to commit it
-        apiKey: "b2b9d3c7d9be346b7754344b8c3bccdd",
-
-        indexName: "ZUPLO_SITE",
-
-        // Optional: see doc section below
-        contextualSearch: true,
-
-        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        //externalUrlRegex: "external\\.com|domain\\.com",
-
-        // Optional: Algolia search parameters
-        searchParameters: {},
-
-        // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: "/docs/search",
-
-        //... other Algolia params
-      },
       navbar: {
         logo: {
           alt: "Zuplo",
