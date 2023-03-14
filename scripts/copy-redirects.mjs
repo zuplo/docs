0ad896ea182c5f616c057eaf2476ddc7b5255636
Nathan Totten <nathan@ntotten.com>
2023-03-14 13:47:14 -0400

    redirects

--- scripts/copy-redirects.mjs
+++
@@ -1,7 +0,0 @@
-import { copyFileSync } from "fs";
-import { resolve } from "path";
-
-const sourcePath = resolve(process.cwd(), "./_redirects");
-const destinationPath = resolve(process.cwd(), "./build/_redirects");
-
-copyFileSync(sourcePath, destinationPath);
