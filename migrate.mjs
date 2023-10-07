87b1763b0fb02991cfa6d7e4c3e70aaea49a5989
Nathan Totten <nathan@ntotten.com>
2023-10-07 13:16:08 -0400

    wip: next migration

---
+++ migrate.mjs
@@ -0,0 +1,24 @@
+import { cp, mkdir } from "fs/promises";
+import glob from "glob";
+import path from "path";
+
+
+const matches= await new Promise((resolve, reject) => {
+  glob(
+    "docs/**/*.md",
+    (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    }
+  );
+});
+
+await Promise.all(matches.map(async (match) => {
+  const currentPath = path.join(process.cwd(), match);
+  const newPath = path.join(process.cwd(), "src/app", match).replace(".md", "")
+  await mkdir(newPath, { recursive: true })
+  cp(currentPath, path.join(newPath, "page.md"))
+}));
