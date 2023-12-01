d81215073401fb106fc3eb94b16ce9eb1c7b6b4d
esemenov <esemenov@ronasit.com>
2023-12-01 15:25:17 +0600

    fix: button styles

--- src/components/StartFreeCTAButton.tsx
+++ src/components/StartFreeCTAButton.tsx
@@ -13,27 +13,21 @@ export const StartFreeCTAButton = ({
   return (
     <>
       <Link
-        className={classNames(
-          "btn hidden rounded-xl px-7 pb-4 text-base text-sm leading-6 md:inline-block",
-          {
-            [`${style}`]: style,
-            "btn-primary-dark": type === "dark",
-            "btn-primary-light": type === "light",
-          },
-        )}
+        className={classNames("btn hidden md:inline-block", {
+          [`${style}`]: style,
+          "btn-primary-dark": type === "dark",
+          "btn-primary-light": type === "light",
+        })}
         href={"https://portal.zuplo.com"}
       >
         Start free
       </Link>
       <Link
-        className={classNames(
-          "btn rounded-xl px-7 pb-4 text-base text-sm leading-6 md:hidden",
-          {
-            [`${style}`]: style,
-            "btn-primary-dark": type === "dark",
-            "btn-primary-light": type === "light",
-          },
-        )}
+        className={classNames("btn md:hidden", {
+          [`${style}`]: style,
+          "btn-primary-dark": type === "dark",
+          "btn-primary-light": type === "light",
+        })}
         href="https://calendly.com/d/y3n-4dg-8xc/call-with-zuplo"
       >
         Book a demo
