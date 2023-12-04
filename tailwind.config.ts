b1f9a2cb361627df6354ddd5643d7a31a83d23ea
esemyonov <132259164+esemyonov@users.noreply.github.com>
2023-12-04 10:15:07 +0600

    feat: header template (#272)
    * feat: header template
    
    * fix: button styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,6 @@ export default {
       fontSize: {
         // xs: ["0.75rem", { lineHeight: "1rem" }],
         // sm: ["0.875rem", { lineHeight: "1.5rem" }],
-        base: ["1rem", { lineHeight: "2rem" }],
         lg: ["1.125rem", { lineHeight: "1.75rem" }],
         xl: ["1.25rem", { lineHeight: "2rem" }],
         "2xl": ["1.5rem", { lineHeight: "2.5rem" }],
