e1b730a04350ef0978ee177065c5868711fcf5cd
Nathan Totten <nathan@ntotten.com>
2023-12-17 14:39:08 -0500

    updated script

--- scripts/get-policies.sh
+++ scripts/get-policies.sh
@@ -1,5 +1,5 @@
 rm -rf temp
-RANDOM=(curl https://uuid.new)
+RANDOM=$(curl https://uuid.new)
 curl https://cdn.zuplo.com/policies/runtime.zip?$RANDOM -o runtime.zip
 curl https://cdn.zuplo.com/policies/graphql.zip?$RANDOM  -o graphql.zip
 unzip -q graphql.zip -d temp 
