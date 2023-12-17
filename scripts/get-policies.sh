4273b809659c9583127f5306602b5ff3f26a7a81
Nathan Totten <nathan@ntotten.com>
2023-12-17 17:45:42 -0500

    updated deprecated generation

--- scripts/get-policies.sh
+++ scripts/get-policies.sh
@@ -1,7 +1,7 @@
 rm -rf temp
 RANDOM=$(curl https://uuid.new)
-curl https://cdn.zuplo.com/policies/runtime.zip?$RANDOM -o runtime.zip
-curl https://cdn.zuplo.com/policies/graphql.zip?$RANDOM  -o graphql.zip
+curl https://cdn.zuplo.com/policies/runtime/policies.zip -o runtime.zip
+curl https://cdn.zuplo.com/policies/graphql/policies.zip -o graphql.zip
 unzip -q graphql.zip -d temp 
 unzip -q runtime.zip -d temp 
 rm graphql.zip
