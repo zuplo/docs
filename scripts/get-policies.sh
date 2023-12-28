509cbd7c1fe439481853d610fcef7df34c2cea27
esemenov <esemenov@ronasit.com>
2023-12-28 15:56:19 +0600

    fix: build fix

--- scripts/get-policies.sh
+++ scripts/get-policies.sh
@@ -2,7 +2,7 @@ rm -rf temp
 RANDOM=$(curl https://uuid.new)
 curl https://cdn.zuplo.com/policies/runtime.zip?$RANDOM -o runtime.zip
 curl https://cdn.zuplo.com/policies/graphql.zip?$RANDOM  -o graphql.zip
-unzip -q graphql.zip -d policies 
-unzip -q runtime.zip -d policies 
+unzip -q -o graphql.zip -d policies 
+unzip -q -o runtime.zip -d policies 
 rm graphql.zip
 rm runtime.zip
