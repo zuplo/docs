91a794abad42cc360516eeb845cad8d61cb79ec8
fivenp <mh@fivenp.com>
2024-01-11 21:40:57 +0100

    chore: migrate to inter (#309)

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -80,7 +80,7 @@ export function TableOfContents({
                       href={`#${section.id}`}
                       className={clsx(
                         isActive(section)
-                          ? 'text-sky-500'
+                          ? 'text-pink'
                           : 'font-normal text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-300',
                       )}
                     >
