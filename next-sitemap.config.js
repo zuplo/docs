774fff57a3d26b6a4375f1777f31c3a06c748f23
Nathan Totten <nathan@ntotten.com>
2024-01-16 12:52:10 -0500

    next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://zuplo.com",
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   exclude: ["/sitemap-*.xml"],
 };
