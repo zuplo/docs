9baefaeffa48e353120a19fb36aed048719348bf
esemenov <esemenov@ronasit.com>
2024-01-25 16:54:44 +0600

    feat: blur filter for header
    refs: https://app.clickup.com/t/86bx6jkau

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,48 +50,50 @@ export default function Header() {
   }, []);
 
   return (
-    <header
-      className={clsx(
-        "max-w-wide sticky top-4 z-50 m-4 flex h-24 w-full flex-none flex-wrap items-center justify-between rounded-xl bg-black/95 px-4 py-5 shadow-md shadow-gray-900/25 transition duration-500 sm:px-6 lg:px-8",
-        isScrolled
-          ? "dark:bg-black/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-black/75"
-          : "dark:bg-black/95",
-      )}
-    >
-      <div className="mr-6 flex lg:hidden">
-        <MobileNavigation />
-      </div>
-      <div className="relative flex basis-0 items-center">
-        <Link href="https://zuplo.com" aria-label="Home page">
-          <Logomark className="h-9 w-9 md:hidden" />
-          <Logo className="hidden h-7 w-auto md:inline-block" />
-        </Link>
-      </div>
-      <div className="relative flex basis-0 items-center justify-end gap-6 sm:gap-8 md:flex-grow">
-        <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
-          <Search />
+    <header className="max-w-wide sticky top-0 pt-4 mb-4 z-50 w-full [@supports(backdrop-filter:blur(0))]:backdrop-blur-[2px]">
+      <div
+        className={clsx(
+          "max-w-wide flex h-24 w-full flex-none flex-wrap items-center justify-between rounded-xl bg-black/95 px-4 py-5 shadow-md shadow-gray-900/25 transition duration-500 sm:px-6 lg:px-8",
+          isScrolled
+            ? "dark:bg-black/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-black/75"
+            : "dark:bg-black/95",
+        )}
+      >
+        <div className="mr-6 flex lg:hidden">
+          <MobileNavigation />
         </div>
-        <div className="hidden items-center gap-10 text-base font-semibold text-white lg:flex">
-          {links.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className={`strong hover:text-pink tracking-wider no-underline outline-none transition-colors ${
-                item.active ? "text-pink" : " text-white"
-              }`}
-              aria-current={item.href === currentPath ? "page" : undefined}
-            >
-              {item.name}
-            </Link>
-          ))}
+        <div className="relative flex basis-0 items-center">
+          <Link href="https://zuplo.com" aria-label="Home page">
+            <Logomark className="h-9 w-9 md:hidden" />
+            <Logo className="hidden h-7 w-auto md:inline-block" />
+          </Link>
         </div>
-        <ThemeSelector className="relative z-10" />
-        <Link
-          href="https://portal.zuplo.com/signup"
-          className="btn btn-primary-dark font-base bg-pink block hidden whitespace-nowrap rounded-lg px-5 py-3 text-center text-sm font-semibold tracking-wider text-white no-underline transition-colors hover:bg-white hover:text-black md:inline-block"
-        >
-          Start free
-        </Link>
+        <div className="relative flex basis-0 items-center justify-end gap-6 sm:gap-8 md:flex-grow">
+          <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
+            <Search />
+          </div>
+          <div className="hidden items-center gap-10 text-base font-semibold text-white lg:flex">
+            {links.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className={`strong hover:text-pink tracking-wider no-underline outline-none transition-colors ${
+                  item.active ? "text-pink" : " text-white"
+                }`}
+                aria-current={item.href === currentPath ? "page" : undefined}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <ThemeSelector className="relative z-10" />
+          <Link
+            href="https://portal.zuplo.com/signup"
+            className="btn btn-primary-dark font-base bg-pink block hidden whitespace-nowrap rounded-lg px-5 py-3 text-center text-sm font-semibold tracking-wider text-white no-underline transition-colors hover:bg-white hover:text-black md:inline-block"
+          >
+            Start free
+          </Link>
+        </div>
       </div>
     </header>
   );
