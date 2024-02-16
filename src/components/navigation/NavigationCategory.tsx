b7a2d87a1dd43cdb7850505732fcfc0eda816f50
Daniel Lehr <daniellehr@gmx.de>
2024-02-16 20:25:02 +0100

    Add some smooth category expand/collapse transitions

--- src/components/navigation/NavigationCategory.tsx
+++ src/components/navigation/NavigationCategory.tsx
@@ -1,7 +1,7 @@
 import { NavItem } from "@/lib/interfaces";
 import { hasActiveNavLinkItem } from "@/lib/utils/has-active-nav-link-item";
 import clsx from "clsx";
-import { ChevronRightIcon, ChevronDownIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { NavigationLinkItem } from "./NavigationLinkItem";
@@ -55,26 +55,26 @@ export function NavigationCategory({ navItem, isRoot }: Props) {
         onClick={onClick}
       >
         <span className="flex-grow">{navItem.label}</span>
-        {isCollapsed ? (
-          <ChevronRightIcon className="h-4 w-4" />
-        ) : (
-          <ChevronDownIcon className="h-4 w-4" />
-        )}
+        <ChevronRightIcon
+          className={clsx("h-4 w-4 duration-200", !isCollapsed && "rotate-90")}
+        />
       </a>
       <ul
         role="list"
         className={clsx([
-          "mt-2 space-y-2 border-l border-gray-100 dark:border-gray-800 lg:mt-4 lg:space-y-4 lg:border-gray-200",
-          isCollapsed && "hidden",
+          "grid duration-200 mt-2 space-y-2 border-l border-gray-100 dark:border-gray-800 lg:mt-4 lg:space-y-4 lg:border-gray-200",
+          isCollapsed ? "grid-rows-[0fr]" : "grid-rows-[1fr]",
         ])}
       >
-        {navItem.items?.map((innerItem) =>
-          innerItem.items?.length ? (
-            <NavigationCategory navItem={innerItem} key={innerItem.label} />
-          ) : (
-            <NavigationLinkItem link={innerItem} key={innerItem.label} />
-          ),
-        )}
+        <div className="overflow-hidden">
+          {navItem.items?.map((innerItem) =>
+            innerItem.items?.length ? (
+              <NavigationCategory navItem={innerItem} key={innerItem.label} />
+            ) : (
+              <NavigationLinkItem link={innerItem} key={innerItem.label} />
+            ),
+          )}
+        </div>
       </ul>
     </li>
   );
