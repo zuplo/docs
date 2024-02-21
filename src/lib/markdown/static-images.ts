5b9b350c62151be2dfa4a3d0e4ea483aae2433dd
Nathan Totten <nathan@ntotten.com>
2024-02-21 14:24:09 -0800

    fixed header link hover

--- src/lib/markdown/static-images.ts
+++ src/lib/markdown/static-images.ts
@@ -1,5 +1,6 @@
+import { Root } from "mdast";
 import path from "path";
-import { Parent } from "unist";
+import { Plugin } from "unified";
 import { visit } from "unist-util-visit";
 import type { VFile } from "vfile";
 
@@ -7,13 +8,17 @@ import type { VFile } from "vfile";
  * Copies images from the /static folder to /public if they are referenced by a
  * @returns
  */
-export default function rehypeStaticImages() {
-  return async (root: Parent, vfile: VFile) => {
+const rehypeStaticImages: Plugin<[], Root, Root> =
+  () => async (root, vfile: VFile) => {
     const promises: Promise<void>[] = [];
     visit(root, ["element"], (node: any, index, parent) => {
       promises.push(
         (async () => {
-          if (node.type === "element" && node.tagName === "img") {
+          if (
+            node.type === "element" &&
+            node.tagName === "img" &&
+            node.properties
+          ) {
             if (!node.properties.src.startsWith("http")) {
               const publicDir = path.join(process.cwd(), "public");
               let url = path.resolve(
@@ -23,16 +28,19 @@ export default function rehypeStaticImages() {
               const relativePath = url.replace(publicDir, "");
               if (process.env.USE_IMAGE_CDN) {
                 node.properties.src = `https://cdn.zuplo.com/docs${relativePath}`;
-                node.properties.srcSet = [
-                  `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=320,format=auto/docs${relativePath}   320w`,
-                  `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=640,format=auto/docs${relativePath}   640w`,
-                  `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=960,format=auto/docs${relativePath}   960w`,
-                  `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=1280,format=auto/docs${relativePath} 1280w`,
-                  `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=2560,format=auto/docs${relativePath} 2560w`,
-                ].join(", ");
               } else {
                 node.properties.src = `/docs${relativePath}`;
               }
+            }
+
+            if (node.properties.src.startsWith("https://cdn.zuplo.com/")) {
+              const url = new URL(node.properties.src);
+              node.properties.srcSet = [
+                `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=640,format=auto${url.pathname}   640w`,
+                `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=960,format=auto${url.pathname}   960w`,
+                `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=1280,format=auto${url.pathname} 1280w`,
+                `https://cdn.zuplo.com/cdn-cgi/image/fit=contain,width=2560,format=auto${url.pathname} 2560w`,
+              ].join(", ");
             }
           }
         })(),
@@ -40,4 +48,4 @@ export default function rehypeStaticImages() {
     });
     await Promise.all(promises);
   };
-}
+export default rehypeStaticImages;
