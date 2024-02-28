f78c6aa9800de577256d6d1c38e4d4d27f89fdaf
Nathan Totten <nathan@ntotten.com>
2024-02-28 12:00:45 -0500

    Added worker header log

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,9 @@ export function middleware(request: NextRequest) {
   const isProduction = process.env.NODE_ENV === "production";
   const requestedHost = request.headers.get("X-Forwarded-Host");
 
+  const workerHeader = request.headers.get("CF-Worker");
+  console.log({ workerHeader });
+
   if (isProduction && requestedHost && !requestedHost.match(/zuplo.com/)) {
     const requestedPort = request.headers.get("X-Forwarded-Port");
     const requestedProto = request.headers.get("X-Forwarded-Proto");
