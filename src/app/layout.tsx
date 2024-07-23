0f37e1fe75f406fa2fd4ad70f6535b825b19b777
Nathan Totten <nathan@ntotten.com>
2024-07-23 18:06:39 -0400

    updated tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import "@/styles/styles.css";
 import "@/styles/tailwind.css";
 import Script from "next/script";
 import { Suspense } from "react";
-import { GOOGLE_TAG_MANAGER_ID, KOALA_URL, ZARAZ_URL } from "../lib/env";
+import { GOOGLE_TAG_MANAGER_ID, KOALA_URL } from "../lib/env";
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
@@ -55,27 +55,17 @@ export default function RootLayout({
         {GOOGLE_TAG_MANAGER_ID && (
           <>
             <Script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_MANAGER_ID}`}
-            />
-            <Script
               id="gtm"
-              strategy="afterInteractive"
               async
               dangerouslySetInnerHTML={{
-                __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${GOOGLE_TAG_MANAGER_ID}');`,
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`,
               }}
             />
           </>
-        )}
-        {ZARAZ_URL && (
-          <Script
-            id="zaraz"
-            strategy="afterInteractive"
-            async
-            src={ZARAZ_URL}
-            referrerPolicy="origin"
-          />
         )}
         {KOALA_URL && (
           <Script
