3d05742cb2aee8a706f338ff9106726158f5c274
Nathan Totten <nate@zuplo.com>
2024-07-23 18:50:59 -0400

    Revert "Added gtm (#410)" (#411)
    This reverts commit 17791195e8a9c047a9e47ff45088300fafdf58ca.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import "@/styles/styles.css";
 import "@/styles/tailwind.css";
 import Script from "next/script";
 import { Suspense } from "react";
-import { GOOGLE_TAG_MANAGER_ID, KOALA_URL } from "../lib/env";
+import { KOALA_URL, ZARAZ_URL } from "../lib/env";
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
@@ -52,20 +52,14 @@ export default function RootLayout({
           type="image/x-icon"
           href="https://cdn.zuplo.com/www/favicon.png"
         />
-        {GOOGLE_TAG_MANAGER_ID && (
-          <>
-            <Script
-              id="gtm"
-              async
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://ta.zuplo.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`,
-              }}
-            />
-          </>
+        {ZARAZ_URL && (
+          <Script
+            id="zaraz"
+            strategy="afterInteractive"
+            async
+            src={ZARAZ_URL}
+            referrerPolicy="origin"
+          />
         )}
         {KOALA_URL && (
           <Script
