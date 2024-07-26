66afc9595346bfaf50a82760b8081abd759baa8d
Nathan Totten <nathan@ntotten.com>
2024-07-26 15:28:18 -0400

    Reapply "Reapply "Reapply "Added gtm (#410)" (#411)""
    This reverts commit dd04d8658d441f1293fabf4e694b2c1e56d8bdf6.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import "@/styles/styles.css";
 import "@/styles/tailwind.css";
 import Script from "next/script";
 import { Suspense } from "react";
-import { KOALA_URL, ZARAZ_URL } from "../lib/env";
+import { GOOGLE_TAG_MANAGER_ID, KOALA_URL } from "../lib/env";
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
@@ -52,14 +52,20 @@ export default function RootLayout({
           type="image/x-icon"
           href="https://cdn.zuplo.com/www/favicon.png"
         />
-        {ZARAZ_URL && (
-          <Script
-            id="zaraz"
-            strategy="afterInteractive"
-            async
-            src={ZARAZ_URL}
-            referrerPolicy="origin"
-          />
+        {GOOGLE_TAG_MANAGER_ID && (
+          <>
+            <Script
+              id="gtm"
+              async
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://ta.zuplo.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`,
+              }}
+            />
+          </>
         )}
         {KOALA_URL && (
           <Script
