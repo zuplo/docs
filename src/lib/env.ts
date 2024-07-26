d9664dbcd55425d250759862b64bafa3193fbffb
Nathan Totten <nathan@ntotten.com>
2024-07-26 12:51:30 -0400

    Reapply "Reapply "Added gtm (#410)" (#411)"
    This reverts commit 9eaba3580c0a6f639af3b3cd856b5e7469b9bf23.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,3 +2,5 @@ export const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 export const POSTHOG_URL = process.env.NEXT_PUBLIC_POSTHOG_URL;
 export const ZARAZ_URL = process.env.NEXT_PUBLIC_ZARAZ_URL;
 export const KOALA_URL = process.env.NEXT_PUBLIC_KOALA_URL;
+export const GOOGLE_TAG_MANAGER_ID =
+  process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
