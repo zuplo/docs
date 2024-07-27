9948f129d974b086e7fc8bf90aefbf05cec7751d
Nathan Totten <nathan@ntotten.com>
2024-07-27 18:51:16 -0400

    updated analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,8 @@ import localFont from "next/font/local";
 
 import "@/styles/styles.css";
 import "@/styles/tailwind.css";
-import Script from "next/script";
 import { Suspense } from "react";
-import { GOOGLE_TAG_MANAGER_ID, KOALA_URL } from "../lib/env";
+import { Analytics } from "./analytics";
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
@@ -52,33 +51,7 @@ export default function RootLayout({
           type="image/x-icon"
           href="https://cdn.zuplo.com/www/favicon.png"
         />
-        {GOOGLE_TAG_MANAGER_ID && (
-          <>
-            <Script
-              id="gtm"
-              async
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://ta.zuplo.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`,
-              }}
-            />
-          </>
-        )}
-        {KOALA_URL && (
-          <Script
-            id="koala-analytics"
-            strategy="afterInteractive"
-            async
-            dangerouslySetInnerHTML={{
-              __html: `
-          window.koalaSettings = { host: '${KOALA_URL}' };
-          !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","${KOALA_URL}/v1/pk_32d64a435a311ccc9462e3721dba58cb3e35/sdk.js"),(document.body || document.head).appendChild(n)}();`,
-            }}
-          />
-        )}
+        <Analytics />
       </head>
       <body className="flex min-h-full bg-white dark:bg-black">
         <Suspense>
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,8 @@
+// Analytics
 export const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 export const POSTHOG_URL = process.env.NEXT_PUBLIC_POSTHOG_URL;
-export const ZARAZ_URL = process.env.NEXT_PUBLIC_ZARAZ_URL;
-export const KOALA_URL = process.env.NEXT_PUBLIC_KOALA_URL;
+export const REDDIT_PIXEL_ID = process.env.NEXT_PUBLIC_REDDIT_PIXEL_ID;
 export const GOOGLE_TAG_MANAGER_ID =
   process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+export const TWITTER_PIXEL_ID = process.env.NEXT_PUBLIC_TWITTER_PIXEL_ID;
+export const KOALA_URL = process.env.NEXT_PUBLIC_KOALA_URL;
