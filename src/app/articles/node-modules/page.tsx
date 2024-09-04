4e63c6f121db88746e64244c56694b612a68d7d7
Nathan Totten <nathan@ntotten.com>
2024-09-04 11:45:54 -0400

    Added note about type checking

--- src/app/articles/node-modules/page.tsx
+++ src/app/articles/node-modules/page.tsx
@@ -1,5 +1,6 @@
 import BundlesTable from "@/components/BundlesTable";
 import { DocsLayout } from "@/components/DocsLayout";
+import Callout from "../../../components/markdown/Callout";
 
 export const metadata = {
   title: "Node Modules",
@@ -20,14 +21,30 @@ export default async function Page() {
       </p>
       <p>
         Additionally, you can also bundle custom modules inside of your own
-        project. This process does require some knowledge of node and npm,
-        but it allows you to use any module or version of the module. To learn
-        how to bundle your own modules, see the sample{" "}
+        project. This process does require some knowledge of node and npm, but
+        it allows you to use any module or version of the module. To learn how
+        to bundle your own modules, see the sample{" "}
         <a href="https://github.com/zuplo/zuplo/tree/main/examples/custom-module">
           Custom Modules on Github
         </a>
         .
       </p>
+      <Callout type="caution" title="Type Checking Errors">
+        <p>
+          When referencing a custom module in the Zuplo Portal, you might see
+          that the module has a red underline in your code editor. Some modules
+          are not typed or their types cannot be bundled to use with the Zuplo
+          Portal.
+        </p>
+        <p>
+          You can ignore these errors as they will not affect the functionality
+          of the module. Alternatively, if you use{" "}
+          <a href="/docs/articles/local-development">local development</a> you
+          can install the module locally to enable code completion and type
+          checking.
+        </p>
+      </Callout>
+
       <p>Below are the currently installed modules.</p>
       <BundlesTable />
     </DocsLayout>
