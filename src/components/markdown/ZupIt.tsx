dfe6a0f6a774b39ddeb6e26a78c311d2793152f7
Adrian Machado <amachado@uwaterloo.ca>
2024-09-04 19:30:05 -0700

    Fix zupit link

--- src/components/markdown/ZupIt.tsx
+++ src/components/markdown/ZupIt.tsx
@@ -9,7 +9,7 @@ export default function ZupIt({ repoUrl }: { repoUrl: string }) {
         This sample is available on on <Link href={repoUrl}>GitHub</Link> or
         click the button below to open the code directly in the portal.{" "}
       </p>
-      <Link href={`http://portal.zuplo.com/clone?sourceRepoUrl=${repoUrl}`}>
+      <Link href={`https://portal.zuplo.com/clone?sourceRepoUrl=${repoUrl}`}>
         <img src="https://cdn.zuplo.com/www/zupit.svg" alt="ZupIt"></img>
       </Link>
     </Callout>
