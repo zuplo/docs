5b218bd41b18e708abdbcb91bbe4385bf91a254a
Adrian Machado <amachado@uwaterloo.ca>
2024-09-05 14:51:08 -0700

    Slashless canonical

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://zuplo.com/docs/",
+  siteUrl: "https://zuplo.com/docs",
   generateRobotsTxt: false,
   exclude: ["/sitemap-*.xml"],
 };
