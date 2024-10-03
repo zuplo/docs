71cea43d66aa9ffb76226b70a073b4dbc33dca74
Adrian Machado <amachado@uwaterloo.ca>
2024-10-03 15:05:22 -0700

    Sitemap tweak

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: "https://zuplo.com/docs",
   generateRobotsTxt: false,
-  exclude: ["/sitemap-*.xml", "/docs/policies/api-key-auth-inbound"],
+  exclude: ["/sitemap-*.xml", "/policies/api-key-auth-inbound"],
 };
