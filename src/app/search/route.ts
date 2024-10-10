bfec3aa416a95870cbec8825490bfe352d4999fb
Daniel Lehr <daniellehr@gmx.de>
2024-10-10 15:31:00 +0200

    Switch to Zudoku

--- src/app/search/route.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { type NextRequest } from "next/server";
-
-export function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("q");
-  query ? redirect(`/docs?search=${query}`) : redirect("/docs");
-}
