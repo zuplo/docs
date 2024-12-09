548ed774b555896a991603a03379b425662876ac
Daniel Lehr <daniellehr@gmx.de>
2024-12-09 21:06:29 +0100

    try zudoku skew protection

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import vercelSkewProtection from "vite-plugin-vercel-skew-protection";
-
-export default {
-  plugins: [
-    process.env.VERCEL_SKEW_PROTECTION_ENABLED === "1" &&
-      vercelSkewProtection(),
-  ].filter(Boolean),
-};
