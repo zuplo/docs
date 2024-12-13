c69d49f3cb9896678465da63e9ded27a54c1723b
Nathan Totten <nathan@ntotten.com>
2024-12-13 08:38:51 -0500

    Added sentry

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
+  plugins: [
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "zuplo",
+      project: "docs",
+    }),
+  ],
+});
