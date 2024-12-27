4923f956deec334a5d641054879912c12ef3da8b
Nathan Totten <nathan@ntotten.com>
2024-12-27 17:27:21 -0600

    updated network diagrams

--- src/diagrams/CustomerIngressWithManagedDedicated.tsx
+++ src/diagrams/CustomerIngressWithManagedDedicated.tsx
@@ -1,119 +1,59 @@
 import { Position } from "@xyflow/react";
-import Diagram, { DiagramNode, Edge } from "./common/Diagram.js";
-
 import React from "react";
+import Diagram, { DiagramNode, Edge } from "./common/Diagram.js";
+import { createIngressEdgressEdge } from "./edges.js";
+import {
+  createBackend,
+  createClient,
+  createCustomNode,
+  createVpcGroupNode,
+  createZuploApiNode,
+} from "./nodes.js";
+
 type _react = typeof React;
 
+const clientNode = createClient({ position: { x: 10, y: 90 } });
+const backendNode = createBackend({
+  position: { x: 200, y: 150 },
+  ingress: Position.Right,
+});
+const zuploApiNode = createZuploApiNode({
+  position: { x: 425, y: 90 },
+  ingress: Position.Left,
+  egress: Position.Bottom,
+});
+const wafNode = createCustomNode({
+  position: { x: 210, y: 90 },
+  label: "WAF",
+  ingress: Position.Left,
+  egress: Position.Right,
+});
+const zuploVpcNode = createVpcGroupNode({
+  position: { x: 400, y: 40 },
+  label: "Zuplo VPC",
+  width: 180,
+  height: 100,
+});
+const customerVpcNode = createVpcGroupNode({
+  position: { x: 150, y: 40 },
+  label: "Customer VPC",
+  width: 180,
+  height: 160,
+});
+
 const initialNodes: DiagramNode[] = [
-  {
-    id: "end-user",
-    type: "custom",
-    position: { x: 10, y: 90 },
-    data: {
-      label: "End User",
-      handles: [
-        {
-          id: "egress",
-          type: "source",
-          position: Position.Right,
-        },
-      ],
-    },
-  },
-  {
-    id: "waf",
-    type: "custom",
-    position: { x: 210, y: 90 },
-    data: {
-      label: "WAF",
-      handles: [
-        {
-          id: "ingress",
-          type: "target",
-          position: Position.Left,
-        },
-        {
-          id: "egress",
-          type: "source",
-          position: Position.Right,
-        },
-      ],
-    },
-  },
-  {
-    id: "backend",
-    type: "custom",
-    position: { x: 200, y: 150 },
-    data: {
-      label: "Backend",
-      handles: [
-        {
-          id: "ingress",
-          type: "target",
-          position: Position.Right,
-        },
-      ],
-    },
-  },
-  {
-    id: "zuplo-api",
-    type: "zuplo",
-    position: { x: 425, y: 90 },
-    style: { zIndex: 20 },
-    data: {
-      label: "Hello",
-      handles: [
-        {
-          id: "ingress",
-          type: "target",
-          position: Position.Left,
-        },
-        {
-          id: "egress",
-          type: "source",
-          position: Position.Bottom,
-        },
-      ],
-    },
-  },
-  {
-    id: "4",
-    data: { label: "Zuplo VPC" },
-    position: { x: 400, y: 50 },
-    style: { width: 180, height: 100, zIndex: -1 },
-    type: "labeled-group",
-  },
-  {
-    id: "5",
-    data: { label: "Customer VPC" },
-    position: { x: 150, y: 50 },
-    style: { width: 180, height: 150, zIndex: -1 },
-    type: "labeled-group",
-  },
+  clientNode,
+  wafNode,
+  backendNode,
+  zuploApiNode,
+  zuploVpcNode,
+  customerVpcNode,
 ];
 
 const initialEdges: Edge[] = [
-  {
-    id: "user-to-waf",
-    source: "end-user",
-    target: "waf",
-    targetHandle: "ingress",
-    sourceHandle: "egress",
-  },
-  {
-    id: "zuplo-to-backend",
-    source: "zuplo-api",
-    target: "backend",
-    targetHandle: "ingress",
-    sourceHandle: "egress",
-  },
-  {
-    id: "waf-to-zuplo",
-    source: "waf",
-    target: "zuplo-api",
-    targetHandle: "ingress",
-    sourceHandle: "egress",
-  },
+  createIngressEdgressEdge({ egress: clientNode, ingress: wafNode }),
+  createIngressEdgressEdge({ egress: wafNode, ingress: zuploApiNode }),
+  createIngressEdgressEdge({ egress: zuploApiNode, ingress: backendNode }),
 ];
 
 export default function ZuploIngressWithManagedDedicated() {
---
+++ src/diagrams/nodes.ts
@@ -0,0 +1,132 @@
+import { Position, XYPosition } from "@xyflow/react";
+import { CustomHandleProps } from "./common/CustomNode.js";
+import { DiagramNode } from "./common/Diagram.js";
+
+export const createClient = ({
+  position,
+}: {
+  position: XYPosition;
+}): DiagramNode => ({
+  id: "client",
+  type: "custom",
+  position,
+  data: {
+    label: "Client",
+    handles: [
+      {
+        id: "egress",
+        type: "source",
+        position: Position.Right,
+      },
+    ],
+  },
+});
+
+export const createBackend = ({
+  position,
+  ingress,
+}: {
+  position: XYPosition;
+  ingress: Position;
+}): DiagramNode => ({
+  id: "backend",
+  type: "custom",
+  position,
+  data: {
+    label: "Backend",
+    handles: [
+      {
+        id: "ingress",
+        type: "target",
+        position: ingress,
+      },
+    ],
+  },
+});
+
+export const createCustomNode = ({
+  position,
+  egress,
+  ingress,
+  label,
+}: {
+  position: XYPosition;
+  egress?: Position;
+  ingress?: Position;
+  label: string;
+}): DiagramNode => ({
+  id: `node-${label.toLowerCase().replaceAll(" ", "-")}`,
+  type: "custom",
+  position,
+  data: {
+    label,
+    handles: [
+      ...(ingress
+        ? [
+            {
+              id: "ingress",
+              type: "target",
+              position: ingress,
+            } satisfies CustomHandleProps,
+          ]
+        : []),
+      ...(egress
+        ? [
+            {
+              id: "egress",
+              type: "source",
+              position: egress,
+            } satisfies CustomHandleProps,
+          ]
+        : []),
+    ],
+  },
+});
+
+export const createZuploApiNode = ({
+  position,
+  egress,
+  ingress,
+}: {
+  position: XYPosition;
+  egress: Position;
+  ingress: Position;
+}): DiagramNode => ({
+  id: "zuplo-api",
+  type: "zuplo",
+  position,
+  style: { zIndex: 20 },
+  data: {
+    label: "Hello",
+    handles: [
+      {
+        id: "ingress",
+        type: "target",
+        position: ingress,
+      },
+      {
+        id: "egress",
+        type: "source",
+        position: egress,
+      },
+    ],
+  },
+});
+
+export const createVpcGroupNode = ({
+  position,
+  label,
+  width,
+  height,
+}: {
+  position: XYPosition;
+  label: string;
+  width: number;
+  height: number;
+}) => ({
+  id: `vpc-group-${label.toLowerCase().replaceAll(" ", "-")}`,
+  data: { label },
+  position,
+  style: { width, height, zIndex: -1 },
+  type: "labeledGroup",
+});
