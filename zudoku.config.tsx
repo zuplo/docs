f98c66bd56f56ff726ab3ddd3401042db531d05d
Moritz <moritz@schwrr.com>
2025-02-08 15:48:44 +0100

    add portal to top navigation

--- zudoku.config.tsx
+++ zudoku.config.tsx
@@ -61,6 +61,10 @@ const config: ZudokuConfig = {
       label: "Documentation",
     },
     {
+      id: "dev-portal",
+      label: "Developer Portal",
+    },
+    {
       id: "policies",
       label: "Policies & Handlers",
     },
