58d04a72b2c3531a8c1be33d0b6c12af27cdcab5
Martyn Davies <martynrdavies@gmail.com>
2025-03-14 17:22:11 +0100

    Add Aserto to Authorization sidebar (#532)
    * add Aserto to Authorization sidebar
    
    * update sidebar with authzen

--- sidebar.ts
+++ sidebar.ts
@@ -427,6 +427,8 @@ export const policies: SidebarEntry = [
     type: "category",
     label: "Authorization",
     items: [
+      "policies/aserto-authz-inbound",
+      "policies/authzen-inbound",
       "policies/okta-fga-authz-inbound",
       "policies/openfga-authz-inbound",
       "policies/axiomatics-authz-inbound",
