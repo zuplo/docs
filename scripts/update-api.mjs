6b6552a0afbc6125604fb11a0ade62d9f2c560ff
Nathan Totten <nathan@ntotten.com>
2025-03-27 13:25:03 -0400

    update api url

--- scripts/update-api.mjs
+++ scripts/update-api.mjs
@@ -3,7 +3,7 @@ import { writeFileSync } from "fs";
 const response = await fetch("https://dev.zuplo.com/openapi");
 const data = await response.json();
 
-data.servers[0].url = "https://api.zuplo.com";
+data.servers[0].url = "https://dev.zuplo.com";
 data.servers[0].description = "Zuplo API";
 
 writeFileSync("./api.json", JSON.stringify(data, null, 2), "utf-8");
