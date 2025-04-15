1c368ae5638ff2ded0300d33af4111b6a5b5e8e7
Daniel Lehr <daniellehr@gmx.de>
2025-04-15 08:34:06 +0200

    Optimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   build: {
-    sourcemap: true, // Source map generation must be turned on
+    sourcemap: true,
   },
   plugins: [
+    ViteImageOptimizer({
+      exclude: /\.(gif|svg)$/,
+    }),
     // Put the Sentry vite plugin after all other plugins
     process.env.VERCEL &&
       sentryVitePlugin({
