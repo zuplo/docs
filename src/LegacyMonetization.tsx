e984555ed1ff5e35563cd93b5b1a9fe384047132
Martyn Davies <martyn@zuplo.com>
2025-07-29 18:02:39 +0200

    Legacy Dev Portal: Update warning with date and details (#686)
    * update warning component with more details and date
    
    * update monetization component with date

--- src/LegacyMonetization.tsx
+++ src/LegacyMonetization.tsx
@@ -4,9 +4,13 @@ export function LegacyMonetization() {
   return (
     <Callout type="danger" title={"Legacy Feature"}>
       <p>
-        The monetization feature is no longer available for new projects. We
-        will have more information about the new monetization features in the
-        future. If you have questions, please reach out to us at{" "}
+        The monetization feature is no longer available for new projects and
+        will be sunset on November 15, 2025. Please see our{" "}
+        <a href="/docs/articles/monetization">Monetization documentation</a> for
+        the new, more flexible solution and integration with OpenMeter.
+      </p>
+      <p style={{ marginTop: "1rem" }}>
+        If you have questions, please reach out to us at{" "}
         <a href="mailto:support@zuplo.com">support@zuplo.com</a>.
       </p>
     </Callout>
