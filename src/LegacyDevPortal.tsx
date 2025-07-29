ab2b95380a528bd7fc1a7a101e74b079c13ca78a
Martyn Davies <martynrdavies@gmail.com>
2025-07-29 16:53:27 +0200

    update warning component with more details and date

--- src/LegacyDevPortal.tsx
+++ src/LegacyDevPortal.tsx
@@ -5,8 +5,16 @@ export function LegacyDevPortal() {
     <Callout type="danger" title={"Legacy Feature"}>
       <p>
         This version of the developer portal is no longer availible to new
-        projects. If you are looking for the new developer portal, please refer
-        to the <a href="/docs/dev-portal/introduction">docs</a>.
+        projects and will be sunset on November 15, 2025. If you are looking for
+        the new developer portal, please refer to the{" "}
+        <a href="/docs/dev-portal/introduction">docs</a>.
+      </p>
+      <p style={{ marginTop: "1rem" }}>
+        If you are using the legacy developer portal, please{" "}
+        <a href="/docs/dev-portal/migration">
+          migrate to the new Zudoku powered portal experience
+        </a>{" "}
+        before November 15, 2025.
       </p>
     </Callout>
   );
