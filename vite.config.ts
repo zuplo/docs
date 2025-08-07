41fe35d6d0068c3106360fe2d01b44fc4c7773e3
Nathan Totten <nate@zuplo.com>
2025-08-07 15:32:41 -0400

    Configure vercel microfrontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
-import { microfrontends } from '@vercel/microfrontends/experimental/vite';
+import { microfrontends } from "@vercel/microfrontends/experimental/vite";
 
 export default defineConfig({
   build: {
     sourcemap: true, // Source map generation must be turned on
   },
   plugins: [
-    microfrontends({ basePath: '/docs' })
+    microfrontends({ basePath: "/docs" }),
     // Put the Sentry vite plugin after all other plugins
     process.env.VERCEL &&
       sentryVitePlugin({
