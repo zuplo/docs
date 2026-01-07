53a9e035ccbbbc6c6f9cce13e3c1231118ee3d42
Nick Chen <nick@zuplo.com>
2026-01-07 11:16:18 -0800

    ZUP-5499: Expose Metering Preview (#870)

--- scripts/update-api.mjs
+++ scripts/update-api.mjs
@@ -1,14 +1,26 @@
 import { writeFileSync } from "fs";
 
-const response = await fetch("https://dev.zuplo.com/openapi", {
-  headers: {
-    Accept: "application/json",
-    "Accept-Encoding": "identity",
-  },
-});
-const data = await response.json();
+const [response, responseBeta] = await Promise.all([
+  fetch("https://dev.zuplo.com/openapi", {
+    headers: {
+      Accept: "application/json",
+      "Accept-Encoding": "identity",
+    },
+  }),
+  fetch("https://dev.zuplo.com/openapi-preview", {
+    headers: {
+      Accept: "application/json",
+      "Accept-Encoding": "identity",
+    },
+  }),
+]);
 
+const data = await response.json();
 data.servers[0].url = "https://dev.zuplo.com";
 data.servers[0].description = "Zuplo API";
-
 writeFileSync("./api.json", JSON.stringify(data, null, 2), "utf-8");
+
+const dataBeta = await responseBeta.json();
+dataBeta.servers[0].url = "https://dev.zuplo.com";
+dataBeta.servers[0].description = "Zuplo API (Preview)";
+writeFileSync("./api-preview.json", JSON.stringify(dataBeta, null, 2), "utf-8");
